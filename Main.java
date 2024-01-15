94dec5a1db3f1235096431d276a7e5b3c7d4fa16
hostguy <112008657+hostgay@users.noreply.github.com>
2024-01-15 11:24:48 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,27 +1,29 @@
+
 public class Main {
     public static void main(String[] args) {
-        ageAndTemperature(36, -6);
-        ageAndTemperature(15, 1);
-        ageAndTemperature(26, -4);
-        ageAndTemperature(56, 32);
-        ageAndTemperature(12, -12);
+        test(25, 15);
+        test(18, 25);
+        test(50, 5);
+        test(generateRandomAge(), 20);
+        test(generateRandomAge(), 30);
+    }
 
+    public static void test(int age, int temperature) {
+        String result = canGoOutside(age, temperature);
+        System.out.println(result);
     }
-    public static String ageAndTemperature(int age, double temperature) {
-     if (age >= 20 && age <=45 ){
-         if (temperature >= -20 && temperature <= 30)
-        System.out.println("можно идти гулять");
-       }  else if ( age < 20 ) {
-        if (temperature > 1 && temperature < 9) {
-            System.out.println("Можно идти гулять");
-        }
-    } else if (age >= 46) {
-        if (temperature > -11 && temperature < 25) {
-            System.out.println("Можно идти гулять");
+
+    public static String canGoOutside(int age, int temperature) {
+        if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30)
+                || (age < 20 && temperature >= 0 && temperature <= 28)
+                || (age > 45 && temperature >= -10 && temperature <= 25)) {
+            return "Можно идти гулять";
         } else {
-            System.out.println("Оставайтесь дома!");
+            return "Оставайтесь дома";
         }
     }
-        return "Возраст человека = " + age + " Температура на улице = " + temperature;
+
+    public static int generateRandomAge() {
+        return (int) (Math.random() * 70) + 1;
     }
 }
